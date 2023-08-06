36f9248994dd7ae6f66aa7d6f9026c3058ca08f4
Your Name <youremail@domain.com>
2023-08-06 14:52:42 +0200

    First Step

---
+++ script.js
@@ -0,0 +1,6 @@
+let slider = document.getElementById('slider');
+let nextBtn = document.getElementById('nextbtn');
+let prevBtn = document.getElementById('prevbtn');
+
+let sliderImageArr = ['https://soliloquywp.com/wp-content/uploads/2019/04/nb_sss_8.jpg','https://soliloquywp.com/wp-content/uploads/2019/04/nb_sss_4.jpg','https://soliloquywp.com/wp-content/uploads/2019/04/stunning-spring-sliders.jpg'];
+
